729157ab4441b828b27f93d6f424fb18e5157dc7
Manudominguez1994 <manudomnav1994@gmail.com>
2023-09-04 22:55:11 +0200

    Filter Genre Done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h2>MAURI RECORDS</h2>
+      <Link to={"/allVinyls"}>Accede a la tienda de vinilos</Link>
+
+
+    </div>
   )
 }
 
--- src/pages/vinyl/AllVinyls.jsx
+++ src/pages/vinyl/AllVinyls.jsx
@@ -7,6 +7,8 @@ import { AuthContext } from "../../context/auth.context";
 function AllVinyls() {
   const navigate = useNavigate();
 
+  // const AllGenre = ["Rock", "Pop", "Hip-Hop", "Jazz", "Electronica", "Soul", "Reagge","Otros"]
+
   const { activeUserId } = useContext(AuthContext);
   const [allVinyls, setAllVinyls] = useState([]);
 
@@ -23,6 +25,41 @@ function AllVinyls() {
       navigate("/error");
     }
   };
+
+  const handlefilterRock = () => {
+    const rockVinyls = allVinyls.filter((vinyl)=> vinyl.genre === "Rock");
+    setAllVinyls(rockVinyls);
+  }
+  const handlefilterPop = () => {
+    const popVinyls = allVinyls.filter((vinyl)=> vinyl.genre === "Pop");
+    setAllVinyls(popVinyls);
+  }
+  const handlefilterHipHop = () => {
+    const hiphopVinyls = allVinyls.filter((vinyl)=> vinyl.genre === "Hip-Hop");
+    setAllVinyls(hiphopVinyls);
+  }
+  const handlefilterJazz = () => {
+    const jazzVinyls = allVinyls.filter((vinyl)=> vinyl.genre === "Jazz");
+    setAllVinyls(jazzVinyls);
+  }
+  const handlefilterElectronica = () => {
+    const electronicaVinyls = allVinyls.filter((vinyl)=> vinyl.genre === "Electronica");
+    setAllVinyls(electronicaVinyls);
+  }
+  const handlefilterSoul = () => {
+    const soulVinyls = allVinyls.filter((vinyl)=> vinyl.genre === "Soul");
+    setAllVinyls(soulVinyls);
+  }
+  const handlefilterReagge = () => {
+    const reaggeVinyls = allVinyls.filter((vinyl)=> vinyl.genre === "Reagge");
+    setAllVinyls(reaggeVinyls);
+  }
+  const handlefilterOther = () => {
+    const otherVinyls = allVinyls.filter((vinyl)=> vinyl.genre === "Otros");
+    setAllVinyls(otherVinyls);
+  }
+
+  
   const handleAddFavorite = async (vinylId) => {
     try {
       await service.put(`/user/${vinylId}/fav`);
@@ -32,8 +69,36 @@ function AllVinyls() {
     }
   };
 
+  if(allVinyls.length === 0){
+    return(
+      <div>
+    <h3>No hay vinilos de este genero</h3>
+    <Link onClick={getAllVinyls}><h5>Todos los vinilos</h5></Link> 
+    </div>
+    ) 
+  }
+
   return (
     <div>
+      <div>
+      {/* {AllGenre.map((eachGenre)=>{
+          return(
+            <>
+              <button>{eachGenre}</button>
+            </>
+          )
+      })} */}
+      <button onClick={handlefilterRock}>Rock</button>
+      <button onClick={handlefilterPop}>Pop</button>
+      <button onClick={handlefilterHipHop}>Hip-Hop</button>
+      <button onClick={handlefilterJazz}>Jazz</button>
+      <button onClick={handlefilterElectronica}>Electronica</button>
+      <button onClick={handlefilterSoul}>Soul</button>
+      <button onClick={handlefilterReagge}>Reagge</button>
+      <button onClick={handlefilterOther}>Otros</button>
+      <Link onClick={getAllVinyls}><h5>Todos los vinilos</h5></Link>      
+      </div>
+      <div>
       {allVinyls.map((eachVinyl) => {
         // console.log(eachVinyl.sellerUser)
         // console.log(activeUserId)
@@ -55,6 +120,7 @@ function AllVinyls() {
           </div>
         );
       })}
+      </div>
     </div>
   );
 }
