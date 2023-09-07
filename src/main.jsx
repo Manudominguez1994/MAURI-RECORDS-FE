77f15ea1b5f165ba7e01ea942a1fbf6f0c0e56d3
Manudominguez1994 <manudomnav1994@gmail.com>
2023-09-07 14:32:38 +0200

    hola

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthWrapper } from './context/auth.context.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  <BrowserRouter>
